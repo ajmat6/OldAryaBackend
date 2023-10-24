860be0536aac226946ba7a0fce0d8a0cfab89ae5
Ajmat <ajmat1130666@gmail.com>
2023-10-24 22:01:22 +0530

    User items API + Item delete API of specific user

--- src/routes/items.js
+++ src/routes/items.js
@@ -4,6 +4,7 @@ const Items = require('../models/Items');
 const { userMiddleware } = require('../middleware/userMiddleware');
 const upload = require('../middleware/uploadMiddleware');
 const fetchuser = require('../middleware/fetchuser');
+const { default: mongoose } = require('mongoose');
 
 router.post('/addItem', fetchuser, userMiddleware, upload.array('itemImages'), async (req, res) => {
     try
@@ -42,12 +43,62 @@ router.post('/addItem', fetchuser, userMiddleware, upload.array('itemImages'), a
     }
 })
 
+// get all items:
 router.get('/getItems', fetchuser, userMiddleware, async (req, res) => {
     try
     {
         const allItems = await Items.find({});
         if(allItems) return res.status(200).json(allItems);
         else return res.status(400).json({message: "No Items!"});
+    }
+    catch (error)
+    {
+        console.log(error.message);
+        res.status(500).send("Some Internal Server Error Occured! Please try again after some times");    
+    }
+})
+
+// get items of a particular user:
+router.get('/user/getItems', fetchuser, userMiddleware, async (req, res) => {
+    try
+    {
+        const userItems = await Items.find({userId: req.user.id});
+        if(userItems) return res.status(200).json(userItems);
+        else return res.status(400).json({message: "No Items!"});
+    }
+    catch (error)
+    {
+        console.log(error.message);
+        res.status(500).send("Some Internal Server Error Occured! Please try again after some times");    
+    }
+})
+
+// get item by its id:
+router.get('/getItem/:id', fetchuser, userMiddleware, async (req, res) => {
+    try
+    {
+        const {id} = req.params;
+        const _id = new mongoose.Types.ObjectId(id)
+        const item = await Items.find({_id: _id})
+        if(item) return res.status(200).json(item);
+        else return res.status(400).json({message: "Oh! swap, something went wrong!"});
+    }
+    catch (error)
+    {
+        console.log(error.message);
+        res.status(500).send("Some Internal Server Error Occured! Please try again after some times");    
+    }
+})
+
+// delete item:
+router.delete('/deleteItem/:id', fetchuser, userMiddleware, async (req, res) => {
+    try
+    {
+        const {id} = req.params;
+        const _id = new mongoose.Types.ObjectId(id)
+        const item = await Items.findOneAndDelete({_id: _id})
+        if(item) return res.status(200).json(item);
+        else return res.status(400).json({message: "Oh! swap, something went wrong!"});
     }
     catch (error)
     {
